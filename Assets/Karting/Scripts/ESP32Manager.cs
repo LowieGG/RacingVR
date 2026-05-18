efc199662f39e0d9fafe48fa2b5592d3e075fcfb
LouisM159 <louis.mathij@student.kuleuven.be>
2026-05-18 14:03:53 +0200

    geluiden

--- Assets/Karting/Scripts/ESP32Manager.cs
+++ Assets/Karting/Scripts/ESP32Manager.cs
@@ -8,6 +8,20 @@ public class ESP32Manager : MonoBehaviour
     public string comPoort = "COM3";
     public int baudRate = 9600;
 
+    [Header("Audio Instellingen")]
+    public AudioClip nitroSound;
+    public AudioClip smsSound;
+    public AudioClip ejectSound;
+    public AudioClip honkSound;
+    public AudioClip jumpSound;
+    public AudioClip lichtenSound;
+    public AudioClip wiperSound;
+    public AudioClip laserSound;
+    public AudioClip gasSound;
+    public AudioClip remSound;
+    
+    private AudioSource audioSource;
+
     private SerialPort serialPort;
     private Thread leesThread;
     private string laasteData = "";
@@ -24,11 +38,31 @@ public class ESP32Manager : MonoBehaviour
     public bool gasIngedrukt = false;
     public bool remIngedrukt = false;
 
+    // Vorige staat van knoppen voor edge detection
+    private bool vorige_nitro = false;
+    private bool vorige_sms = false;
+    private bool vorige_eject = false;
+    private bool vorige_honk = false;
+    private bool vorige_jump = false;
+    private bool vorige_lichten = false;
+    private bool vorige_wiper = false;
+    private bool vorige_laser = false;
+    private bool vorige_gas = false;
+    private bool vorige_rem = false;
+
     // Rotary encoder - later toevoegen
     public float stuurHoek = 0f;
 
     void Start()
     {
+        // Setup AudioSource
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+        audioSource.playOnAwake = false;
+
         serialPort = new SerialPort(comPoort, baudRate);
         serialPort.Open();
         draait = true;
@@ -54,55 +88,105 @@ public class ESP32Manager : MonoBehaviour
 
     void Update()
     {
+        // NITRO
         if (laasteData.Contains("NITRO:1"))
             nitroIngedrukt = true;
         else if (laasteData.Contains("NITRO:0"))
             nitroIngedrukt = false;
+        
+        if (nitroIngedrukt && !vorige_nitro && nitroSound != null)
+            audioSource.PlayOneShot(nitroSound);
+        vorige_nitro = nitroIngedrukt;
 
+        // SMS
         if (laasteData.Contains("SMS:1"))
             smsKnopIngedrukt = true;
         else if (laasteData.Contains("SMS:0"))
             smsKnopIngedrukt = false;
+        
+        if (smsKnopIngedrukt && !vorige_sms && smsSound != null)
+            audioSource.PlayOneShot(smsSound);
+        vorige_sms = smsKnopIngedrukt;
 
+        // EJECT
         if (laasteData.Contains("EJECT:1"))
             ejectIngedrukt = true;
         else if (laasteData.Contains("EJECT:0"))
             ejectIngedrukt = false;
+        
+        if (ejectIngedrukt && !vorige_eject && ejectSound != null)
+            audioSource.PlayOneShot(ejectSound);
+        vorige_eject = ejectIngedrukt;
 
+        // HONK
         if (laasteData.Contains("HONK:1"))
             honkIngedrukt = true;
         else if (laasteData.Contains("HONK:0"))
             honkIngedrukt = false;
+        
+        if (honkIngedrukt && !vorige_honk && honkSound != null)
+            audioSource.PlayOneShot(honkSound);
+        vorige_honk = honkIngedrukt;
 
+        // JUMP
         if (laasteData.Contains("JUMP:1"))
             jumpIngedrukt = true;
         else if (laasteData.Contains("JUMP:0"))
             jumpIngedrukt = false;
+        
+        if (jumpIngedrukt && !vorige_jump && jumpSound != null)
+            audioSource.PlayOneShot(jumpSound);
+        vorige_jump = jumpIngedrukt;
 
+        // LICHTEN
         if (laasteData.Contains("LICHT:1"))
             lichtenIngedrukt = true;
         else if (laasteData.Contains("LICHT:0"))
             lichtenIngedrukt = false;
+        
+        if (lichtenIngedrukt && !vorige_lichten && lichtenSound != null)
+            audioSource.PlayOneShot(lichtenSound);
+        vorige_lichten = lichtenIngedrukt;
 
+        // WIPER
         if (laasteData.Contains("WIPER:1"))
             wiperSchakelaar = true;
         else if (laasteData.Contains("WIPER:0"))
             wiperSchakelaar = false;
+        
+        if (wiperSchakelaar && !vorige_wiper && wiperSound != null)
+            audioSource.PlayOneShot(wiperSound);
+        vorige_wiper = wiperSchakelaar;
 
+        // LASER
         if (laasteData.Contains("LASER:1"))
             laserIngedrukt = true;
         else if (laasteData.Contains("LASER:0"))
             laserIngedrukt = false;
+        
+        if (laserIngedrukt && !vorige_laser && laserSound != null)
+            audioSource.PlayOneShot(laserSound);
+        vorige_laser = laserIngedrukt;
 
+        // GAS
         if (laasteData.Contains("GAS:1"))
             gasIngedrukt = true;
         else if (laasteData.Contains("GAS:0"))
             gasIngedrukt = false;
+        
+        if (gasIngedrukt && !vorige_gas && gasSound != null)
+            audioSource.PlayOneShot(gasSound);
+        vorige_gas = gasIngedrukt;
 
+        // REM
         if (laasteData.Contains("REM:1"))
             remIngedrukt = true;
         else if (laasteData.Contains("REM:0"))
             remIngedrukt = false;
+        
+        if (remIngedrukt && !vorige_rem && remSound != null)
+            audioSource.PlayOneShot(remSound);
+        vorige_rem = remIngedrukt;
 
         // Rotary encoder - later toevoegen
         // if (laasteData.Contains("STUUR:"))
